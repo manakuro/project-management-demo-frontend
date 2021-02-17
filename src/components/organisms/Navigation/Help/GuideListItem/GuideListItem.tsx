75eb4efc4e78d27b16db078fa1b208767bff6aba
manato kuroda <a7108100@gmail.com>
2021-02-17 16:05:49 +0900

    refactor: split GuideListItem into ListItem and ListItemDetail component

--- src/components/organisms/Navigation/Help/GuideListItem/GuideListItem.tsx
+++ src/components/organisms/Navigation/Help/GuideListItem/GuideListItem.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import {
-  AspectRatio,
-  Box,
-  Flex,
-  Icon,
-  IconType,
-  Text,
-} from 'src/components/atoms'
-import { transitions } from 'src/styles'
+import { IconType } from 'src/components/atoms'
 import { useDisclosure } from '@chakra-ui/react'
+import { ListItem } from './ListItem'
+import { ListItemDetail } from './ListItemDetail'
 
 export type Item = {
   number: number
@@ -30,105 +24,17 @@ type Props = {
   item: Item
 }
 
-const PADDING_X = 4
+export const PADDING_X = 4
 export const GuideListItem: React.VFC<Props> = (props) => {
   const { isOpen, onToggle } = useDisclosure()
-
   const { item } = props
-  const icon = item.done ? item.iconDone : item.icon
 
   return (
     <>
       {isOpen ? (
-        <Flex
-          flexDirection="column"
-          borderRadius="md"
-          minH="340px"
-          color="gray.700"
-          bg="help.guide.bg"
-        >
-          <Flex
-            w="full"
-            px={PADDING_X}
-            py={2}
-            onClick={onToggle}
-            cursor="pointer"
-          >
-            <Flex
-              borderRadius="50%"
-              bg="gray.800"
-              justifyContent="center"
-              alignItems="center"
-              w="20px"
-              h="20px"
-              mr={PADDING_X}
-              fontSize="xs"
-              fontWeight="bold"
-              color="white"
-            >
-              {item.number}
-            </Flex>
-            <Text fontSize="sm" fontWeight="bold" flex={1}>
-              {item.title}
-            </Text>
-            <Icon icon="chevronDown" />
-          </Flex>
-          <AspectRatio ratio={4 / 3} w="full" cursor="pointer">
-            <Flex
-              bg="gray.200"
-              w="full"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Icon icon="play" w={16} h={16} />
-            </Flex>
-          </AspectRatio>
-          <Flex p={PADDING_X} flexDirection="column">
-            <Text fontSize="sm" isTruncated noOfLines={3}>
-              {item.description}
-            </Text>
-            <Box>
-              <Text
-                as="span"
-                fontSize="xs"
-                color="link"
-                cursor="pointer"
-                _hover={{
-                  textDecoration: 'underline !important',
-                }}
-              >
-                See more
-              </Text>
-            </Box>
-            <Text fontSize="xs" color="text.muted" mt={2} textAlign="right">
-              {item.time}
-            </Text>
-          </Flex>
-        </Flex>
+        <ListItemDetail item={item} onToggle={onToggle} />
       ) : (
-        <Flex
-          px={PADDING_X}
-          py={2}
-          cursor="pointer"
-          bg="rgba(255,255,255,.04)"
-          border="1px"
-          borderColor="navigation.hover"
-          borderRadius="md"
-          alignItems="center"
-          _hover={{
-            bg: 'navigation.hover',
-          }}
-          transition={transitions.base}
-          transitionProperty="background"
-          height="40px"
-          onClick={onToggle}
-        >
-          <Icon icon={icon.name} color={icon.color} mr={PADDING_X} />
-          <Text fontSize="sm" fontWeight="bold" flex={1}>
-            {item.title}
-          </Text>
-          <Icon icon="chevronRight" />
-        </Flex>
+        <ListItem item={item} onToggle={onToggle} />
       )}
     </>
   )
---
+++ src/components/organisms/Navigation/Help/GuideListItem/ListItemDetail.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { AspectRatio, Box, Flex, Icon, Text } from 'src/components/atoms'
+import { PADDING_X, Item } from './GuideListItem'
+
+type Props = {
+  item: Item
+  onToggle: () => void
+}
+
+export const ListItemDetail: React.VFC<Props> = (props) => {
+  const { item, onToggle } = props
+
+  return (
+    <Flex
+      flexDirection="column"
+      borderRadius="md"
+      minH="340px"
+      color="gray.700"
+      bg="help.guide.bg"
+    >
+      <Flex w="full" px={PADDING_X} py={2} onClick={onToggle} cursor="pointer">
+        <Flex
+          borderRadius="50%"
+          bg="gray.800"
+          justifyContent="center"
+          alignItems="center"
+          w="20px"
+          h="20px"
+          mr={PADDING_X}
+          fontSize="xs"
+          fontWeight="bold"
+          color="white"
+        >
+          {item.number}
+        </Flex>
+        <Text fontSize="sm" fontWeight="bold" flex={1}>
+          {item.title}
+        </Text>
+        <Icon icon="chevronDown" />
+      </Flex>
+      <AspectRatio ratio={4 / 3} w="full" cursor="pointer">
+        <Flex
+          bg="gray.200"
+          w="full"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Icon icon="play" w={16} h={16} />
+        </Flex>
+      </AspectRatio>
+      <Flex p={PADDING_X} flexDirection="column">
+        <Text fontSize="sm" isTruncated noOfLines={3}>
+          {item.description}
+        </Text>
+        <Box>
+          <Text
+            as="span"
+            fontSize="xs"
+            color="link"
+            cursor="pointer"
+            _hover={{
+              textDecoration: 'underline !important',
+            }}
+          >
+            See more
+          </Text>
+        </Box>
+        <Text fontSize="xs" color="text.muted" mt={2} textAlign="right">
+          {item.time}
+        </Text>
+      </Flex>
+    </Flex>
+  )
+}

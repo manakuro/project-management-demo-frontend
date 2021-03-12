02fb2f7954db959d397230b545aecef03597e754
manato kuroda <a7108100@gmail.com>
2021-03-13 07:50:49 +0900

    chore: test date picker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,20 @@ import { RecoilRoot } from 'recoil'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'src/styles'
 import { Modals } from 'src/components/organisms'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+import enLocale from 'date-fns/locale/en-US'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme} resetCSS>
-        <>
-          <Component {...pageProps} />
-          <Modals />
-        </>
+        <MuiPickersUtilsProvider utils={DateFnsUtils} locale={enLocale}>
+          <>
+            <Component {...pageProps} />
+            <Modals />
+          </>
+        </MuiPickersUtilsProvider>
       </ChakraProvider>
     </RecoilRoot>
   )
--- src/pages/Home/Component.tsx
+++ src/pages/Home/Component.tsx
@@ -1,12 +1,24 @@
-import React, { memo } from 'react'
+import React, { memo, useCallback, useState } from 'react'
 import { Layout, MainHeader } from 'src/components/organisms'
 import { Head } from 'src/components/atoms/Head'
 import { Heading, Box, Stack } from 'src/components/atoms'
 import { TasksDueSoon } from './TasksDueSoon'
+import DateFnsUtils from '@date-io/date-fns'
+import { Calendar, DatePicker } from '@material-ui/pickers'
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date'
 
 type Props = {}
 
+const dateFns = new DateFnsUtils()
 export const Component: React.VFC<Props> = memo<Props>((props) => {
+  const [
+    selectedDate,
+    setSelectedDate,
+  ] = useState<MaterialUiPickersDate | null>(dateFns.date())
+  const handleDateChange = useCallback((date: MaterialUiPickersDate) => {
+    setSelectedDate(date)
+  }, [])
+
   return (
     <Layout>
       <Head title="Home" />
@@ -18,7 +30,11 @@ export const Component: React.VFC<Props> = memo<Props>((props) => {
       <Box w="840px" mx="auto" py={10}>
         <Stack spacing={10}>
           <TasksDueSoon />
-          <Box>hey</Box>
+          <Calendar
+            date={selectedDate}
+            onChange={handleDateChange}
+            disablePast
+          />
         </Stack>
       </Box>
     </Layout>

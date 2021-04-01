8b99c18c1b837d86a799e3da724dc2b17a2c60c0
manato kuroda <a7108100@gmail.com>
2021-04-02 07:30:41 +0900

    feat(my tasks): add menu to attachment icon

--- src/components/organisms/TaskDetail/TaskDetailHeader/Attachment/Attachment.tsx
+++ src/components/organisms/TaskDetail/TaskDetailHeader/Attachment/Attachment.tsx
@@ -1,24 +1,44 @@
 import React from 'react'
 import { Icon, IconButton } from 'src/components/atoms'
 import { Tooltip } from 'src/components/molecules'
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  MenuGroup,
+} from 'src/components/organisms'
 
 type Props = {}
 
 export const Attachment: React.FC<Props> = () => {
   return (
-    <Tooltip
-      hasArrow
-      label="Add a file to this task"
-      aria-label="Attachment button"
-      size="md"
-      openDelay={500}
-    >
-      <IconButton
-        aria-label="attachment file"
-        icon={<Icon icon="attach" color="text.muted" />}
-        variant="ghost"
-        size="sm"
-      />
-    </Tooltip>
+    <Menu isLazy>
+      <Tooltip
+        hasArrow
+        label="Add a file to this task"
+        aria-label="Attachment button"
+        size="md"
+        withIcon
+        openDelay={500}
+      >
+        <MenuButton
+          aria-label="Attachment button"
+          as={IconButton}
+          icon={<Icon icon="attach" color="text.muted" />}
+          size="sm"
+          variant="ghost"
+        />
+      </Tooltip>
+      <MenuList>
+        <MenuGroup title="Attach a File">
+          <MenuItem>Your computer</MenuItem>
+          <MenuItem>Dropbox</MenuItem>
+          <MenuItem>Google Drive</MenuItem>
+          <MenuItem>Box</MenuItem>
+          <MenuItem>OneDrive/SharePoint</MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
   )
 }

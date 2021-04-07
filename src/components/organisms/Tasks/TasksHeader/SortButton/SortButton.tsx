3943f11a781e6d6f06a99ed93fc914bdb9404500
manato kuroda <a7108100@gmail.com>
2021-04-07 10:56:41 +0900

    refactor: use MenuSelect

--- src/components/organisms/Tasks/TasksHeader/SortButton/SortButton.tsx
+++ src/components/organisms/Tasks/TasksHeader/SortButton/SortButton.tsx
@@ -1,47 +1,47 @@
-import React, { memo, useCallback, useState } from 'react'
+import React, { memo, useCallback } from 'react'
 import { Button, Icon } from 'src/components/atoms'
-import { Menu, MenuButton } from 'src/components/organisms'
-import { useDisclosure } from 'src/shared/chakra'
-import { NONE, ListStatus } from './listState'
-import { MenuList } from './MenuList'
+import {
+  MenuButton,
+  MenuItemOption,
+  MenuSelect,
+  MenuSelectList,
+} from 'src/components/organisms'
+import { NONE, ListStatus, PROJECT, DUE_DATE, LIKES } from './listState'
 
 type Props = {}
 
 export const SortButton: React.VFC<Props> = memo<Props>((props) => {
-  const { onOpen, isOpen, onClose } = useDisclosure()
-  const [listState, setListState] = useState<ListStatus>(NONE)
-
-  const handleOpen = useCallback(() => {
-    onOpen()
-  }, [onOpen])
-
-  const handleChangeSort = useCallback(
-    (status: ListStatus) => {
-      setListState(status)
-      onClose()
-    },
-    [onClose],
-  )
+  const handleChange = useCallback((listStatus: ListStatus) => {
+    console.log('hey: ', listStatus)
+  }, [])
 
   return (
-    <Menu isOpen={isOpen} isLazy placement="bottom-end">
-      <MenuButton
-        variant="ghost"
-        aria-label="Sort tasks"
-        as={Button}
-        leftIcon={<Icon icon="sort" />}
-        size="xs"
-        onClick={handleOpen}
-      >
-        Sort
-      </MenuButton>
-      {isOpen && (
-        <MenuList
-          listStatus={listState}
-          onCloseMenu={onClose}
-          onChange={handleChangeSort}
-        />
+    <MenuSelect<ListStatus> onChange={handleChange} placement="bottom-end">
+      {({ isOpen, onOpen, listStatus, onChange, onClose }) => (
+        <>
+          <MenuButton
+            variant="ghost"
+            aria-label="Sort tasks"
+            as={Button}
+            leftIcon={<Icon icon="sort" />}
+            size="xs"
+            onClick={onOpen}
+          >
+            Sort
+          </MenuButton>
+          <MenuSelectList
+            isOpen={isOpen}
+            listStatus={listStatus}
+            onCloseMenu={onClose}
+            onChange={onChange}
+          >
+            <MenuItemOption value={NONE}>None</MenuItemOption>
+            <MenuItemOption value={PROJECT}>Project</MenuItemOption>
+            <MenuItemOption value={DUE_DATE}>Due date</MenuItemOption>
+            <MenuItemOption value={LIKES}>Likes</MenuItemOption>
+          </MenuSelectList>
+        </>
       )}
-    </Menu>
+    </MenuSelect>
   )
 })

84cf8ac283a1a7154b1043f55aed804f369108bc
manato kuroda <a7108100@gmail.com>
2021-04-10 09:21:26 +0900

    refactor(editor): fix margin

--- src/components/organisms/Modals/EditorLinkModal/EditorLinkModal.tsx
+++ src/components/organisms/Modals/EditorLinkModal/EditorLinkModal.tsx
@@ -5,20 +5,17 @@ import { useEditorLinkModal } from './useEditorLinkModal'
 
 type Props = {}
 
+const MARGIN = 30
 export const EditorLinkModal: React.VFC<Props> = (props) => {
   const { isOpen, x, y, onClose } = useEditorLinkModal()
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="xs">
-      <ModalContent position="fixed" top={x} left={y} mb={0} mt={0}>
+      <ModalContent position="fixed" top={x + MARGIN} left={y} mb={0} mt={0}>
         <ModalBody>
           <Stack spacing={2}>
-            <Input
-              focusBorderColor="primary"
-              placeholder="Add text"
-              size="sm"
-            />
-            <Input focusBorderColor="primary" placeholder="Add URL" size="sm" />
+            <Input focusBorderColor="none" placeholder="Add text" size="sm" />
+            <Input focusBorderColor="none" placeholder="Add URL" size="sm" />
           </Stack>
         </ModalBody>
       </ModalContent>

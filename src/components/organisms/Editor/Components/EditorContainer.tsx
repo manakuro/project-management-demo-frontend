caf58076e958c9d6aa6b9d06dcf32d5f9891d748
manato kuroda <a7108100@gmail.com>
2021-04-15 10:19:31 +0900

    refactor(editor): remove ChangeHandler

--- src/components/organisms/Editor/Components/EditorContainer.tsx
+++ src/components/organisms/Editor/Components/EditorContainer.tsx
@@ -1,14 +1,13 @@
+import React, { PropsWithChildren, useEffect, useMemo } from 'react'
 import {
   createJSONTransformer,
   ProsemirrorTransformer,
 } from 'src/shared/prosemirror/transformers'
 import { Node as ProsemirrorNode, Schema } from 'prosemirror-model'
 import { Plugin } from 'prosemirror-state'
-import React, { useState } from 'react'
-
-import { ChangeHandler } from './ChangeHandler'
-import { EditorProvider } from './EdiorProvider'
+import { EditorProvider, useEditorState } from './EdiorProvider'
 import { Portals } from './Portals'
+import { useDebounce } from 'src/hooks'
 
 type Props = {
   schema: Schema
@@ -18,23 +17,37 @@ type Props = {
   debounce?: number
 }
 export const EditorContainer: React.FC<Props> = (props) => {
-  const [transformer] = useState<ProsemirrorTransformer>(() =>
-    createJSONTransformer(props.schema),
+  const transformer = useMemo<ProsemirrorTransformer>(
+    () => createJSONTransformer(props.schema),
+    [props.schema],
   )
-
-  const [initialDoc] = useState<ProsemirrorNode>(() =>
-    transformer.parse(props.value),
+  const initialDoc = useMemo<ProsemirrorNode>(
+    () => transformer.parse(props.value),
+    [props.value, transformer],
   )
 
   return (
     <EditorProvider plugins={props.plugins} doc={initialDoc}>
-      <ChangeHandler
-        transformer={transformer}
-        onChange={props.onChange}
-        debounce={props.debounce}
-      />
-      {props.children}
+      <Container transformer={transformer} {...props} />
       <Portals />
     </EditorProvider>
   )
+}
+
+type ContainerProps<P> = {
+  onChange: (value: P) => void
+  transformer: ProsemirrorTransformer<P>
+  debounce?: number
+}
+export const Container = <P extends unknown>(
+  props: PropsWithChildren<ContainerProps<P>>,
+) => {
+  const state = useEditorState()
+  const debouncedDoc = useDebounce(state.doc, props.debounce)
+
+  useEffect(() => {
+    props.onChange(props.transformer.serialize(debouncedDoc))
+  }, [props, debouncedDoc])
+
+  return <>{props.children}</>
 }

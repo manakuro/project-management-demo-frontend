2c38d07b258043b916cf6ac190dc4b73c3739483
manato kuroda <a7108100@gmail.com>
2021-04-22 09:26:21 +0900

    refactor(editor): add PopoverProfile

--- src/components/organisms/Popovers/PopoverProfile/PopoverProfile.tsx
+++ src/components/organisms/Popovers/PopoverProfile/PopoverProfile.tsx
@@ -16,6 +16,7 @@ import {
   Link,
   Image,
 } from 'src/components/atoms'
+import { PortalManager } from '@chakra-ui/react'
 
 type Props = {
   profile: {
@@ -27,62 +28,68 @@ type Props = {
 
 export const PopoverProfile: React.FC<Props> = (props) => {
   return (
-    <Popover trigger="hover" isLazy>
-      <PopoverTrigger>
-        <Link>{props.children}</Link>
-      </PopoverTrigger>
-      <Portal>
-        <PopoverContent w={56} border="none">
-          <PopoverBody p={0} boxShadow="md" borderRadius="md">
-            <AspectRatio ratio={4 / 3}>
-              <Flex
-                bg="teal.400"
-                w="full"
-                justifyContent="flex-start"
-                alignItems="flex-end !important"
-                borderTopRadius="md"
-              >
+    <PortalManager zIndex={1500}>
+      <Popover trigger="hover" isLazy>
+        <PopoverTrigger>
+          <Link>{props.children}</Link>
+        </PopoverTrigger>
+        <Portal>
+          <PopoverContent w={56} border="none">
+            <PopoverBody p={0} boxShadow="md" borderRadius="md">
+              <AspectRatio ratio={4 / 3}>
                 <Flex
-                  w="full"
-                  justifyContent="center"
-                  position="absolute"
                   bg="teal.400"
-                >
-                  <Image src={props.profile.image} w="100%" objectFit="cover" />
-                </Flex>
-                <Text
                   w="full"
-                  fontSize="sm"
-                  fontWeight="bold"
-                  color="white"
-                  px={3}
-                  py={1}
-                  zIndex="docked"
-                  bgGradient="linear(to-b, transparent, gray.700)"
+                  justifyContent="flex-start"
+                  alignItems="flex-end !important"
+                  borderTopRadius="md"
                 >
-                  {props.profile.name}
+                  <Flex
+                    w="full"
+                    justifyContent="center"
+                    position="absolute"
+                    bg="teal.400"
+                  >
+                    <Image
+                      src={props.profile.image}
+                      w="100%"
+                      objectFit="cover"
+                    />
+                  </Flex>
+                  <Text
+                    w="full"
+                    fontSize="sm"
+                    fontWeight="bold"
+                    color="white"
+                    px={3}
+                    py={1}
+                    zIndex="docked"
+                    bgGradient="linear(to-b, transparent, gray.700)"
+                  >
+                    {props.profile.name}
+                  </Text>
+                </Flex>
+              </AspectRatio>
+              <Flex px={4} py={3} alignItems="center">
+                <Icon icon="mailOutline" w={4} h={4} />
+                <Text fontSize="xs" ml={3} noOfLines={1}>
+                  {props.profile.email}
                 </Text>
               </Flex>
-            </AspectRatio>
-            <Flex px={4} py={3} alignItems="center">
-              <Icon icon="mailOutline" w={4} h={4} />
-              <Text fontSize="xs" ml={3} noOfLines={1}>
-                {props.profile.email}
-              </Text>
-            </Flex>
-            <Divider />
-            <Flex px={4} py={3} alignItems="center">
-              <Button
-                leftIcon={<Icon icon="messageRoundedDots" w={4} h={4} />}
-                variant="outline"
-                size="xs"
-              >
-                Send message
-              </Button>
-            </Flex>
-          </PopoverBody>
-        </PopoverContent>
-      </Portal>
-    </Popover>
+              <Divider />
+              <Flex px={4} py={3} alignItems="center">
+                <Button
+                  leftIcon={<Icon icon="messageRoundedDots" w={4} h={4} />}
+                  variant="outline"
+                  size="xs"
+                >
+                  Send message
+                </Button>
+              </Flex>
+            </PopoverBody>
+          </PopoverContent>
+        </Portal>
+      </Popover>
+    </PortalManager>
   )
 }

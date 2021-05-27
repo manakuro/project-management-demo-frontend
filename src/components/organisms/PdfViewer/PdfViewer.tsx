a9d15efadaf12e46a2cf57e109da4c9b53f6360e
manato kuroda <a7108100@gmail.com>
2021-05-27 16:05:49 +0900

    refactor(pdfviewer): make version fixed for worker url

--- src/components/organisms/PdfViewer/PdfViewer.tsx
+++ src/components/organisms/PdfViewer/PdfViewer.tsx
@@ -8,18 +8,22 @@ import {
 } from '@react-pdf-viewer/core'
 
 import '@react-pdf-viewer/core/lib/styles/index.css'
+const packageJson = require('../../../../package.json')
+const version = packageJson.dependencies['pdfjs-dist']
 
 const characterMap: CharacterMap = {
   isCompressed: true,
   // The url has to end with "/"
-  url: 'https://unpkg.com/pdfjs-dist@2.7.570/cmaps/',
+  url: `https://unpkg.com/pdfjs-dist@${version}/cmaps/`,
 }
 
 type Props = ViewerProps
 
 export const PdfViewer: React.VFC<Props> = (props) => {
   return (
-    <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.7.570/pdf.worker.min.js">
+    <Worker
+      workerUrl={`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.min.js`}
+    >
       <Box w="70%" h="full" className="pdfViewer">
         <Viewer characterMap={characterMap} {...props} />
       </Box>

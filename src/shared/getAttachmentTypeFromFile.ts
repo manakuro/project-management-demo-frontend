b7d8dad9a4d1987b4c76549aaf69b08ff0e3432d
manato kuroda <a7108100@gmail.com>
2021-05-28 14:17:21 +0900

    refactor(task detail): fix getAttachmentTypeFromFile

--- src/shared/getAttachmentTypeFromFile.ts
+++ src/shared/getAttachmentTypeFromFile.ts
@@ -1,12 +1,17 @@
 import {
   ATTACHMENT_TYPE_IMAGE,
-  ATTACHMENT_TYPE_FILE,
+  ATTACHMENT_TYPE_PDF,
+  ATTACHMENT_TYPE_TEXT,
 } from 'src/store/attachments/types'
 
 export const getAttachmentTypeFromFile = (
   fileType: string,
-): typeof ATTACHMENT_TYPE_IMAGE | typeof ATTACHMENT_TYPE_FILE => {
+):
+  | typeof ATTACHMENT_TYPE_IMAGE
+  | typeof ATTACHMENT_TYPE_PDF
+  | typeof ATTACHMENT_TYPE_TEXT => {
   if (fileType.includes('image')) return ATTACHMENT_TYPE_IMAGE
+  if (fileType.includes('pdf')) return ATTACHMENT_TYPE_PDF
 
-  return ATTACHMENT_TYPE_FILE
+  return ATTACHMENT_TYPE_TEXT
 }
--- src/store/attachments/types.ts
+++ src/store/attachments/types.ts
@@ -1,10 +1,10 @@
 export const ATTACHMENT_TYPE_IMAGE = 1 as const
-export const ATTACHMENT_TYPE_FILE = 2 as const
+export const ATTACHMENT_TYPE_PDF = 2 as const
 export const ATTACHMENT_TYPE_TEXT = 3 as const
 
 export type AttachmentType =
   | typeof ATTACHMENT_TYPE_IMAGE
-  | typeof ATTACHMENT_TYPE_FILE
+  | typeof ATTACHMENT_TYPE_PDF
   | typeof ATTACHMENT_TYPE_TEXT
 
 export const ATTACHMENT_STATUS_ATTACHED = 1 as const

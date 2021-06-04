ce3477724adf72576684129f4efac18c5aabf7ee
manato kuroda <a7108100@gmail.com>
2021-06-04 16:58:56 +0900

    refactor(atoms): add minW and minH to ColorBox

--- src/components/atoms/ColorBox/ColorBox.tsx
+++ src/components/atoms/ColorBox/ColorBox.tsx
@@ -12,19 +12,27 @@ export type ColorBoxProps = Props
 const sizes = {
   lg: {
     w: 5,
+    minW: 5,
     h: 5,
+    minH: 5,
   },
   md: {
     w: 4,
+    minW: 4,
     h: 4,
+    minH: 4,
   },
   sm: {
     w: 3,
+    minW: 3,
     h: 3,
+    minH: 4,
   },
   xs: {
     w: 2,
+    minW: 2,
     h: 2,
+    minH: 2,
   },
 } as const
 type Sizes = keyof typeof sizes

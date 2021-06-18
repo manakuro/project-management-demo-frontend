6d4d73b06ae9deb7292b4079a060b940641a5644
manato kuroda <a7108100@gmail.com>
2021-06-18 17:15:38 +0900

    fix(navigation): fix icon position

--- src/components/organisms/Navigation/Header/Header.tsx
+++ src/components/organisms/Navigation/Header/Header.tsx
@@ -22,6 +22,7 @@ export const Header: React.VFC<Props> = memo<Props>(() => {
       alignItems="center"
       px={PADDING_X}
       justifyContent="flex-end"
+      ml={isExpanded ? 0 : '-3px'}
     >
       {isExpanded && (
         <NextLink href="home" passHref>

b6dcfe218a107116a51aab0b567c4ae357607faf
manato kuroda <a7108100@gmail.com>
2021-07-02 09:36:16 +0900

    feat(task detail): use LikeTaskButton for task detail

--- src/components/organisms/TaskDetail/TaskDetailHeader/Like/Like.tsx
+++ src/components/organisms/TaskDetail/TaskDetailHeader/Like/Like.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Icon, IconButton } from 'src/components/atoms'
-import { Tooltip } from 'src/components/molecules'
+import { LikeTaskButton } from 'src/components/molecules'
+import { useTasksListDetail } from 'src/components/organisms'
 
 type Props = {}
 
 export const Like: React.FC<Props> = () => {
-  return (
-    <Tooltip hasArrow label="Like this task" aria-label="Like button" size="sm">
-      <IconButton
-        aria-label="Like this task"
-        icon={<Icon icon="outlineLike" color="text.muted" />}
-        variant="ghost"
-        size="sm"
-      />
-    </Tooltip>
-  )
+  const { taskId } = useTasksListDetail()
+
+  return <LikeTaskButton taskId={taskId} show />
 }

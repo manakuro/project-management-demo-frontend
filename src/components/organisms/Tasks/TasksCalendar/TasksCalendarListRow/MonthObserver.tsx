12dfd818cb82ccf7e9f75b01b77a47b1f1ed80fe
manato kuroda <a7108100@gmail.com>
2021-08-26 15:21:44 +0900

    fix(my tasks): fix month when pressing today button

--- src/components/organisms/Tasks/TasksCalendar/TasksCalendarListRow/MonthObserver.tsx
+++ src/components/organisms/Tasks/TasksCalendar/TasksCalendarListRow/MonthObserver.tsx
@@ -17,22 +17,18 @@ export const MonthObserver: React.FC<Props> = memo<Props>((props) => {
   const { onNextMonth, onPrevMonth } = useTasksCalendarContext()
 
   useEffect(() => {
-    const cleanup = () => {
-      isFirst.current = true
-    }
-
-    if (!isSecondRowOfMonth) return cleanup
+    if (!isSecondRowOfMonth) return
 
     // When scrolling down and the calendar changes to the next month
     if (
       !isFirst.current &&
       !entry?.isIntersecting &&
       entry?.intersectionRatio === 0 &&
-      entry.boundingClientRect.top < 0
+      entry.boundingClientRect.top < 0 && // top is less than 0 when only scrolling
+      entry.boundingClientRect.bottom > 0 // bottom is more than 0 when only scrolling
     ) {
-      // console.log('down!: ', id)
+      console.log('down!: ', id)
       onNextMonth()
-      return cleanup
     }
 
     // When scrolling up and the calendar changes to the previous month
@@ -42,12 +38,13 @@ export const MonthObserver: React.FC<Props> = memo<Props>((props) => {
       entry?.intersectionRatio > 0 &&
       entry.boundingClientRect.top < 0
     ) {
-      // console.log('up!: ', id)
+      console.log('up!: ', id)
       onPrevMonth()
-      return cleanup
     }
 
-    if (entry && isFirst.current) isFirst.current = false
+    if (entry && isFirst.current) {
+      isFirst.current = false
+    }
   }, [entry, isSecondRowOfMonth, id, onNextMonth, onPrevMonth])
 
   return <Flex {...rest} ref={ref} flex={1} />

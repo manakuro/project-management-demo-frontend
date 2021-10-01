b92b713a8108d91b559f9f1b465a8d70c5222d22
manato kuroda <a7108100@gmail.com>
2021-10-01 17:57:07 +0900

    refactor(store): use createState

--- src/store/entities/attachments/atom.ts
+++ src/store/entities/attachments/atom.ts
@@ -1,9 +1,25 @@
-import { atomFamily, selectorFamily, DefaultValue, atom } from 'recoil'
-import { uniqBy } from 'src/shared/utils'
-import { ATTACHMENT_STATUS_ATTACHED } from 'src/store/entities/attachments/types'
+import { selectorFamily } from 'recoil'
+import { createState } from 'src/store/util'
 import { Attachment } from './type'
 
 const key = (str: string) => `src/store/entities/attachments/${str}`
+
+export const initialState = (): Attachment => ({
+  id: '',
+  projectId: '',
+  taskId: '',
+  feedId: '',
+  name: '',
+  src: '',
+  createdAt: '',
+  type: 1,
+  status: 2,
+})
+export const {
+  state: attachmentState,
+  listState: attachmentsState,
+  idsState: attachmentIdsState,
+} = createState({ key, initialState })
 
 export const attachmentIdsByTaskIdState = selectorFamily<string[], string>({
   key: key('attachmentIdsByTaskIdState'),
@@ -22,76 +38,5 @@ export const attachmentIdsByFeedIdState = selectorFamily<string[], string>({
     ({ get }) => {
       const attachments = get(attachmentsState)
       return attachments.filter((p) => p.feedId === feedId).map((p) => p.id)
-    },
-})
-
-export const attachmentIdsState = atom<string[]>({
-  key: key('attachmentIdsState'),
-  default: [],
-})
-export const attachmentIdsGroupByTaskState = atomFamily<string[], string>({
-  key: key('attachmentIdsGroupByTaskState'),
-  default: [],
-})
-export const attachmentsState = atom<Attachment[]>({
-  key: key('attachmentsState'),
-  default: [],
-})
-
-export const initialState = (): Attachment => ({
-  id: '',
-  projectId: '',
-  taskId: '',
-  feedId: '',
-  name: '',
-  src: '',
-  createdAt: '',
-  type: 1,
-  status: 2,
-})
-const state = atomFamily<Attachment, string>({
-  key: key('state'),
-  default: initialState(),
-})
-
-export const attachmentState = selectorFamily<Attachment, string>({
-  key: key('attachmentState'),
-  get:
-    (attachmentId) =>
-    ({ get }) =>
-      get(state(attachmentId)),
-  set:
-    (attachmentId) =>
-    ({ get, set, reset }, newVal) => {
-      if (newVal instanceof DefaultValue) {
-        reset(state(attachmentId))
-        return
-      }
-
-      set(state(attachmentId), newVal)
-      set(attachmentsState, (prev) =>
-        uniqBy([...prev, newVal], 'id').map((p) => {
-          if (p.id === newVal.id) {
-            return {
-              ...p,
-              ...newVal,
-            }
-          }
-          return p
-        }),
-      )
-
-      if (
-        get(attachmentIdsState).find(
-          (attachmentId) => attachmentId === newVal.id,
-        )
-      )
-        return
-
-      set(attachmentIdsState, (prev) => [...prev, newVal.id])
-      set(attachmentIdsGroupByTaskState(newVal.taskId), (prev) => [
-        ...prev,
-        ...(newVal.status === ATTACHMENT_STATUS_ATTACHED ? [newVal.id] : []),
-      ])
     },
 })

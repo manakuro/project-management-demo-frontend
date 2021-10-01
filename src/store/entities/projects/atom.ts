a5bc9a458604b5af1b27e686205bc88b6dca937b
manato kuroda <a7108100@gmail.com>
2021-10-01 17:02:21 +0900

    refactor(store): make the process of creating state object generic

--- src/store/entities/projects/atom.ts
+++ src/store/entities/projects/atom.ts
@@ -1,64 +1,27 @@
-import { atomFamily, selectorFamily, DefaultValue, atom } from 'recoil'
-import { uniqBy } from 'src/shared/utils'
+import { createState } from '../../util'
 import { Project } from './type'
 
 const key = (str: string) => `src/store/entities/projects/${str}`
 
-export const projectIdsState = atom<string[]>({
-  key: key('projectIdsState'),
-  default: [],
-})
-export const projectsState = atom<Project[]>({
-  key: key('projectsState'),
-  default: [],
-})
-
-const state = atomFamily<Project, string>({
-  key: key('state'),
-  default: {
+const initialState = (): Project => ({
+  id: '',
+  name: '',
+  color: {
     id: '',
     name: '',
-    color: {
-      id: '',
-      name: '',
-      color: '',
-    },
-    icon: {
-      id: '',
-    },
-    dueDate: '',
+    color: '',
   },
+  icon: {
+    id: '',
+  },
+  dueDate: '',
 })
 
-export const projectState = selectorFamily<Project, string>({
-  key: key('projectState'),
-  get:
-    (projectId) =>
-    ({ get }) =>
-      get(state(projectId)),
-  set:
-    (projectId) =>
-    ({ get, set, reset }, newVal) => {
-      if (newVal instanceof DefaultValue) {
-        reset(state(projectId))
-        return
-      }
-
-      set(state(projectId), newVal)
-      set(projectsState, (prev) =>
-        uniqBy([...prev, newVal], 'id').map((p) => {
-          if (p.id === newVal.id) {
-            return {
-              ...p,
-              ...newVal,
-            }
-          }
-          return p
-        }),
-      )
-
-      if (get(projectIdsState).find((projectId) => projectId === newVal.id))
-        return
-      set(projectIdsState, (prev) => [...prev, newVal.id])
-    },
+export const {
+  state: projectState,
+  listState: projectsState,
+  idsState: projectIdsState,
+} = createState({
+  key: key('projects'),
+  initialState,
 })
---
+++ src/store/util/createState.ts
@@ -0,0 +1,59 @@
+import { atom, atomFamily, DefaultValue, selectorFamily } from 'recoil'
+import { uniqBy } from 'src/shared/utils'
+
+type Props<T> = {
+  key: string
+  initialState: () => T
+}
+
+type State = {
+  id: string
+}
+export const createState = <T extends State>(props: Props<T>) => {
+  const atomState = atomFamily<T, string>({
+    key: `${props.key}/atomState`,
+    default: props.initialState(),
+  })
+
+  const listState = atom<T[]>({
+    key: `${props.key}/listState`,
+    default: [],
+  })
+
+  const idsState = atom<string[]>({
+    key: `${props.key}/idsState`,
+    default: [],
+  })
+
+  const state = selectorFamily<T, string>({
+    key: `${props.key}/state`,
+    get:
+      (id) =>
+      ({ get }) =>
+        get(atomState(id)),
+    set:
+      (projectId) =>
+      ({ get, set, reset }, newVal) => {
+        if (newVal instanceof DefaultValue) {
+          reset(atomState(projectId))
+          return
+        }
+
+        set(atomState(projectId), newVal)
+        set(listState, (prev) =>
+          uniqBy([...prev, newVal], 'id').map((p) =>
+            p.id === newVal.id ? { ...p, ...newVal } : p,
+          ),
+        )
+
+        if (get(idsState).find((projectId) => projectId === newVal.id)) return
+        set(idsState, (prev) => [...prev, newVal.id])
+      },
+  })
+
+  return {
+    state,
+    listState,
+    idsState,
+  }
+}

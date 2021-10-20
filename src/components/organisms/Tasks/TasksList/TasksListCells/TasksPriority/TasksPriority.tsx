e79525d8246ddd57155a364d98d9713c65d81545
manato kuroda <a7108100@gmail.com>
2021-10-20 09:05:24 +0900

    feat(projects): add icon to priority cell

--- src/components/organisms/Tasks/TasksList/TasksListCells/TasksPriority/TasksPriority.tsx
+++ src/components/organisms/Tasks/TasksList/TasksListCells/TasksPriority/TasksPriority.tsx
@@ -1,7 +1,8 @@
-import React, { memo, useCallback, useState } from 'react'
-import { Flex, FlexProps } from 'src/components/atoms'
+import React, { memo, useCallback, useMemo, useState } from 'react'
+import { Flex, FlexProps, Icon } from 'src/components/atoms'
 import { PriorityChip } from 'src/components/molecules'
 import { TasksListCell } from 'src/components/organisms/Tasks/TasksList/TasksListCell'
+import { useHover } from 'src/hooks/useHover'
 import { useTask } from 'src/store/entities/tasks'
 import { Menu } from './Menu'
 
@@ -14,6 +15,13 @@ export const TasksPriority: React.VFC<Props> = memo<Props>((props) => {
   const { taskId } = props
   const { task } = useTask(taskId)
   const [focused, setFocused] = useState(false)
+  const { ref, isHovering } = useHover()
+  const hasPriority = useMemo(() => task.priority !== 0, [task.priority])
+
+  const showMenuIcon = useMemo(
+    () => !hasPriority && isHovering,
+    [hasPriority, isHovering],
+  )
 
   const handleOpened = useCallback(() => {
     setFocused(true)
@@ -34,10 +42,20 @@ export const TasksPriority: React.VFC<Props> = memo<Props>((props) => {
         position: 'relative',
       }}
       focused={focused}
+      ref={ref}
     >
       <Menu taskId={taskId} onOpened={handleOpened} onClosed={handleClosed}>
         <Flex h="full" flex={1} alignItems="center">
           <PriorityChip taskPriorityType={task.priority} />
+          {showMenuIcon && (
+            <Icon
+              ml="auto"
+              mt="1px"
+              icon="chevronDown"
+              color="text.muted"
+              size="sm"
+            />
+          )}
         </Flex>
       </Menu>
     </TasksListCell>

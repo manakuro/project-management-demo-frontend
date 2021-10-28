0ac3eede0feb3ef7d78a647a62009bc5f6e64fa1
manato kuroda <a7108100@gmail.com>
2021-10-28 16:28:05 +0900

    refactor(workspace): add skeleton holder to overview

--- src/pages/Workspaces/Overview/SkeletonOverview.tsx
+++ src/pages/Workspaces/Overview/SkeletonOverview.tsx
@@ -1,42 +1,70 @@
 import React, { memo } from 'react'
-import { Flex, FlexProps, Skeleton, Stack } from 'src/components/atoms'
-import { TasksHeader, TasksHeaderRight } from 'src/components/organisms/Tasks'
+import { Flex, FlexProps, Grid, Skeleton } from 'src/components/atoms'
+import { OverviewLeft } from './OverviewLeft'
+import { OverviewLeftContent } from './OverviewLeftContent'
+import { OverviewRight } from './OverviewRight'
+import { OverviewRightContent } from './OverviewRightContent'
+import {
+  OverviewSectionHeader,
+  OverviewSectionHeaderHeading,
+} from './OverviewSectionHeader'
 
 type Props = FlexProps
 
 const TEXT_HEIGHT = '16px'
-const BUTTON_HEIGHT = '28px'
 const CARD_HEIGHT = '97px'
 export const SkeletonOverview: React.VFC<Props> = memo<Props>((props) => {
   return (
-    <Flex flex={1} flexDirection="column" {...props}>
-      <TasksHeader
-        h="40px"
-        boxShadow="sm"
-        borderBottom={1}
-        borderStyle="solid"
-        borderColor="gray.200"
-        alignItems="center"
-      >
-        <TasksHeaderRight ml="auto">
-          <Skeleton h={BUTTON_HEIGHT} w="126px" />
-          <Skeleton h={BUTTON_HEIGHT} w="57px" />
-          <Skeleton h={BUTTON_HEIGHT} w="91px" />
-        </TasksHeaderRight>
-      </TasksHeader>
-      <Flex flex={1} p={2}>
-        {[...new Array(3)].map((_, i) => (
-          <Flex flexDirection="column" w="304px" px={3} py={2} key={i}>
-            <Flex h="36px" alignItems="center">
-              <Skeleton h={TEXT_HEIGHT} w="100px" borderRadius="full" />
+    <Flex flex={1} h="full" maxW="full" justifyContent="center" {...props}>
+      <OverviewLeft mt={12}>
+        <OverviewLeftContent>
+          <Flex flexDirection="column">
+            <OverviewSectionHeader>
+              <OverviewSectionHeaderHeading>
+                <Skeleton h={TEXT_HEIGHT} w="100px" borderRadius="full" />
+              </OverviewSectionHeaderHeading>
+            </OverviewSectionHeader>
+            <Flex mt={2} py={1} px={1} mr={-1} ml={-1}>
+              <Skeleton h={CARD_HEIGHT} w="full" borderRadius="md" />
             </Flex>
-            <Stack spacing={2}>
-              <Skeleton h={CARD_HEIGHT} w="full" borderRadius="md" />
-              <Skeleton h={CARD_HEIGHT} w="full" borderRadius="md" />
-            </Stack>
           </Flex>
-        ))}
-      </Flex>
+          <Flex flexDirection="column" mt={8}>
+            <OverviewSectionHeader>
+              <OverviewSectionHeaderHeading>
+                <Skeleton h={TEXT_HEIGHT} w="100px" borderRadius="full" />
+              </OverviewSectionHeaderHeading>
+            </OverviewSectionHeader>
+            <Flex flexDirection="column">
+              {[...new Array(4)].map((_, i) => (
+                <Flex flex={1} py={3} alignItems="center" key={i}>
+                  <Skeleton w="32px" h="32px" borderRadius="full" />
+                  <Skeleton h={TEXT_HEIGHT} flex={1} ml={2} borderRadius="md" />
+                </Flex>
+              ))}
+            </Flex>
+          </Flex>
+        </OverviewLeftContent>
+      </OverviewLeft>
+      <OverviewRight mt={12} pl={8}>
+        <OverviewRightContent>
+          <Flex flexDirection="column">
+            <OverviewSectionHeader>
+              <OverviewSectionHeaderHeading>
+                <Skeleton h={TEXT_HEIGHT} w="100px" borderRadius="full" />
+              </OverviewSectionHeaderHeading>
+            </OverviewSectionHeader>
+            <Flex flexDirection="column">
+              <Flex py={4}>
+                <Grid templateColumns="repeat(4, auto)" gap={6} w="full">
+                  {[...new Array(4)].map((_, i) => (
+                    <Skeleton h="226px" borderRadius="md" key={i} />
+                  ))}
+                </Grid>
+              </Flex>
+            </Flex>
+          </Flex>
+        </OverviewRightContent>
+      </OverviewRight>
     </Flex>
   )
 })

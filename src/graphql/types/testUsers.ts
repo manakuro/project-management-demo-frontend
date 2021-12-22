f9cfa41f8aae0276ee1d5bd453bfe1de1b489c07
manato kuroda <a7108100@gmail.com>
2021-12-22 15:29:46 +0900

    refactor: add graphql/types/testUsers

---
+++ src/graphql/types/testUsers.ts
@@ -0,0 +1,10 @@
+import { TestUsersQuery } from 'src/graphql/types'
+
+export type { TestUsersQuery, TestUsersQueryVariables } from 'src/graphql/types'
+export type {
+  TestUsersQueryHookResult,
+  TestUsersLazyQueryHookResult,
+} from 'src/graphql/hooks'
+
+export type TestUser = NonNullable<EdgesNode<TestUsersQuery['testUsers']>>
+export type TestUsersPageInfo = PageInfo<TestUsersQuery['testUsers']>

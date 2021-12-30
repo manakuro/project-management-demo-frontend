3de9a6b9817bd4505167415d7a9be55f30ff3ea6
manato kuroda <a7108100@gmail.com>
2021-12-30 09:12:27 +0900

    refactor(workspace): add subscription

--- src/store/entities/workspace/hooks/useWorkspace.ts
+++ src/store/entities/workspace/hooks/useWorkspace.ts
@@ -1,11 +1,23 @@
+import { useMemo } from 'react'
 import { useRecoilState } from 'recoil'
+import { useWorkspaceUpdatedSubscription } from 'src/graphql/hooks'
 import { workspaceState } from '../atom'
 
 export const useWorkspace = () => {
-  const [workspace, setWorkspace] = useRecoilState(workspaceState)
+  const [val, setVal] = useRecoilState(workspaceState)
+  const subscriptionResult = useWorkspaceUpdatedSubscription({
+    variables: {
+      id: val.id,
+    },
+    skip: !val.id,
+  })
+
+  const workspace = useMemo(() => {
+    return { ...val, ...(subscriptionResult.data?.workspaceUpdated || {}) }
+  }, [subscriptionResult.data?.workspaceUpdated, val])
 
   return {
     workspace,
-    setWorkspace,
+    setWorkspace: setVal,
   }
 }

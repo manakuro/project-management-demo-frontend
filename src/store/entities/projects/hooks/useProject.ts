ebd180925a4c23cdf964a3a25b6568e5c216796e
manato kuroda <a7108100@gmail.com>
2022-01-21 09:39:38 +0900

    refactor(store/project): add useSubscription

--- src/store/entities/projects/hooks/useProject.ts
+++ src/store/entities/projects/hooks/useProject.ts
@@ -1,26 +1,10 @@
-import { useEffect } from 'react'
 import { useRecoilValue } from 'recoil'
-import { useProjectUpdatedSubscription } from 'src/graphql/hooks'
 import { projectState } from '../atom'
-import { useProjectCommand } from './useProjectCommand'
+import { useSubscription } from './useSubscription'
 
 export const useProject = (projectId: string) => {
-  const { setProjectBySubscription } = useProjectCommand()
   const project = useRecoilValue(projectState(projectId))
-  const subscriptionResult = useProjectUpdatedSubscription({
-    variables: {
-      id: projectId,
-    },
-  })
-
-  useEffect(() => {
-    if (subscriptionResult.loading) return
-    if (!subscriptionResult.data?.projectUpdated) return
-
-    setProjectBySubscription(projectId, subscriptionResult.data.projectUpdated)
-
-    /* eslint react-hooks/exhaustive-deps: off */
-  }, [projectId, subscriptionResult.data?.projectUpdated?.updatedAt])
+  useSubscription(projectId)
 
   return {
     project,

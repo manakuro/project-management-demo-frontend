74bf3798f6e667613e17df5768e415044cb4e7fa
manato kuroda <a7108100@gmail.com>
2022-02-18 13:19:07 +0900

    feat: add task like mutation and subscription

--- src/graphql/types/taskLike.ts
+++ src/graphql/types/taskLike.ts
@@ -3,3 +3,5 @@ import { TaskLikesQuery } from 'src/graphql/types'
 export type TaskLikeResponse = NonNullable<
   EdgesNode<TaskLikesQuery['taskLikes']>
 >
+
+export type { TaskLikeUpdatedSubscription } from 'src/graphql/types'
--- src/store/entities/taskLike/type.ts
+++ src/store/entities/taskLike/type.ts
@@ -1,5 +1,8 @@
 import type { TaskLikeResponse } from 'src/graphql/types/taskLike'
 
-export type { TaskLikeResponse } from 'src/graphql/types/taskLike'
+export type {
+  TaskLikeResponse,
+  TaskLikeUpdatedSubscription as TaskLikeUpdatedSubscriptionResponse,
+} from 'src/graphql/types/taskLike'
 
 export type TaskLike = TaskLikeResponse

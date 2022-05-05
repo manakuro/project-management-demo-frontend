2d13640c03f2b716e3a2cb4abbefdbe7a05f7aee
manato kuroda <a7108100@gmail.com>
2022-05-05 18:00:40 +0900

    chore: add codegen for production

--- scripts/fetchToken/fetchToken.js
+++ scripts/fetchToken/fetchToken.js
@@ -1,16 +1,16 @@
 const fetch = require('node-fetch')
 const body = require('./user.json')
 
+const token = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${token}`
+
 const fetchToken = () =>
-  fetch(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDLmnQHrJp82QbH_gnX7EMj7F-onhwePYY',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify(body),
+  })
 
 module.exports.fetchToken = fetchToken

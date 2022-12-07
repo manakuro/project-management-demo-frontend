0b853ef9467c539497f3d7b69604f2c6ff5c676e
manato <a7108100@gmail.com>
2022-12-07 17:13:49 +0900

    chore: refactor mocks (#49)
    * chore: move testUtils/mock to src/mocks
    
    * chore: fix path for hygen

--- scripts/newQueryMock.ts
+++ scripts/newQueryMock.ts
@@ -7,15 +7,10 @@ const argv = yargs(hideBin(process.argv)).argv as unknown as { file: string }
 spawnSync('yarn tsc:scripts')
 spawnSync(
   argv.file
-    ? `node scripts/graphql/createMock.js --file ${argv.file}`
-    : 'node scripts/graphql/createMock.js',
-)
-spawnSync(
-  argv.file
     ? `node scripts/graphql/createQueryMock.js --file ${argv.file}`
     : 'node scripts/graphql/createQueryMock.js',
 )
 spawnSync(
-  'node scripts/graphql/createQueryMockHandler.js && eslint ./src/testUtils/mock/handlers.ts --fix',
+  'node scripts/graphql/createQueryMockHandler.js && eslint ./src/mocks/queries/handlers.ts --fix',
 )
-spawnSync('eslint ./src/testUtils/mock/queries/ --fix')
+spawnSync('eslint ./src/mocks/queries/ --fix')

411cbdb6d0c1cec8efd8cbf10d44dc12b334e6ef
manato kuroda <a7108100@gmail.com>
2022-12-09 11:00:47 +0900

    chore: add mock date decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,8 @@ import { RouterContext } from "next/dist/shared/lib/router-context";
 import * as msw from 'msw-storybook-addon';
 import {handlers as queryHandlers} from "../src/mocks/queries/handlers";
 import {handlers as mutationHandlers} from "../src/mocks/mutations/handlers";
+import mockdate from 'mockdate'
+import { dateFns } from '../src/shared/dateFns'
 
 msw.initialize()
 
@@ -22,11 +24,29 @@ export const parameters = {
   }
 }
 
+export const mockDateDecorator = (Story, { parameters }) => {
+  const date = parameters.mockDate ?? '2022-11-29T09:16:39+09:00'
+
+  mockdate.set(date)
+
+  const mockedDate = dateFns.format(new Date(date), 'HH:mm:ss dd/MM/yy')
+  console.log('[Mocked date]: ', mockedDate)
+
+  return (
+    <Story />
+  )
+}
+
+
 export const decorators = [
   msw.mswDecorator,
+  mockDateDecorator,
   (Story) => (
     <Provider>
       <Story />
     </Provider>
   ),
 ];
+
+
+
